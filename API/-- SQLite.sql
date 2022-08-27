89bb8c9df62fa7ae0a43736c4436bb006469f53f
Vaibhav Parsana <parsana7v@gmail.com>
2022-08-27 13:51:54 -0400

    fc

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO User (Id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO User (Id, UserName)
+VALUES (2, "Tom");
+
+INSERT INTO User (Id, UserName)
+VALUES (3, "Jane");
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController: ControllerBase
+    {
+        private readonly DataContext _context ;
+        public UsersController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.User.ToListAsync();        
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            return await _context.User.FindAsync(id);
+            
+        }
+
+        
+    }
+}
